12ed7f380643f387c58552c7678bc7105d8fcee3
Ihor <ikruzhynskyy@gmail.com>
2023-06-09 16:28:30 +0300

    added fonts and created a part of footer

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+<<<<<<< HEAD
 import { HashRouter as Router } from 'react-router-dom';
 import { App } from './App';
+=======
+import { App } from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+>>>>>>> 3241e67 (added fonts and create a part of footer)
 
 ReactDOM.render(
   <Router>
