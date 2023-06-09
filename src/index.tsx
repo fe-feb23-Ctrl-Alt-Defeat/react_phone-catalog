a77e7d4cc6e89851355022a0681bcf5285ef31bb
Ihor <ikruzhynskyy@gmail.com>
2023-06-09 16:32:40 +0300

    fixed a mistake

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-<<<<<<< HEAD
-import { HashRouter as Router } from 'react-router-dom';
-import { App } from './App';
-=======
-import { App } from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
->>>>>>> 3241e67 (added fonts and create a part of footer)
+
+import './index.scss';
+import { App } from './App';
 
 ReactDOM.render(
   <Router>
