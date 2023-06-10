8b8b8dba10967b40feb3b00de90a3554d8dea93a
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-06-10 14:13:04 +0300

    header done on other size without BurgerMenu

--- src/compononts/Header/Header.tsx
+++ src/compononts/Header/Header.tsx
@@ -5,6 +5,7 @@ import './header.scss';
 import Logo from '../../images/logo_gnce_gadgets.svg';
 import Favourite from '../../images/icon_favourite.svg';
 import ShoppingBag from '../../images/icon_shopping_bag.svg';
+import Menu_opener from '../../images/icon_menu_opener.svg';
 
 export const Header: React.FC = () => {
   return (
@@ -68,6 +69,7 @@ export const Header: React.FC = () => {
               </li>
             </ul>
           </nav>
+
           <div className="header__right-section">
             <div className="header__favorite">
               {/* фото фейворит */}
@@ -77,12 +79,22 @@ export const Header: React.FC = () => {
                 className="header__favorite-image"
               />
             </div>
+
             <div className="header__shopping-bag">
               {/* иконка корзины */}
               <img
                 src={ShoppingBag}
                 alt="shopping_bag"
                 className="header__shopping-bag-image"
+              />
+            </div>
+
+            <div className="header__menu-opener">
+              {/* иконка menu */}
+              <img
+                src={Menu_opener}
+                alt="menu-opener"
+                className="header__menu-opener-image"
               />
             </div>
           </div>
