762d0bca0ea14ab057636400a72164b78eea000e
Ihor <ikruzhynskyy@gmail.com>
2023-06-11 11:59:31 +0300

    added 404 page not found component

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './pageNotFound.scss';
+import { Link } from 'react-router-dom';
 
 export const PageNotFound = () => {
   return (
-    <div className="pgntf">
-      PageNotFound
+    <div className="error">
+      <div className="error__container">
+        <div className="error__code">404</div>
+        <div className="error__text">Page Not Found</div>
+
+        <button
+          type="button"
+          className="card__buy_button"
+        >
+          <Link to="/" className="card__buy_button_link">
+            home
+          </Link>
+        </button>
+      </div>
     </div>
   );
 };
