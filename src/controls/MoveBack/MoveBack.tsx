c610a6981c212badc6ca742337de2219344adca7
Kyryl <kika_domination@yahoo.com>
2023-06-13 18:29:03 +0300

    add left side of card info

--- src/controls/MoveBack/MoveBack.tsx
+++ src/controls/MoveBack/MoveBack.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './moveBack.scss';
 import IconBack from '../../images/icon_arrow_forward.svg';
 
 export const MoveBack = () => {
+  const navigate = useNavigate();
+
   return (
-    <Link to="/phones" className="move-back">
+    <button
+      type="button"
+      className="move-back"
+      onClick={() => navigate(-1)}
+    >
       <div className="move-back__icon">
         <img src={IconBack} alt="Back icon" className="move-back__icon-image" />
       </div>
       <div className="move-back__paragraph">
         <p className="move-back__paragraph-text">Back</p>
       </div>
-    </Link>
+    </button>
   );
 };
